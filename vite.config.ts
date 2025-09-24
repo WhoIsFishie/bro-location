1ee734de52590f690a30d2491834260dff2bfb28
WhoIsFishie <fishiefish2725@gmail.com>
2025-09-24 14:48:44 +0500

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/shiny-enigma.github.io/',
+  base: '/bro-location/',
 })
